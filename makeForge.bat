7d909f03839d6958961d60809996a51c9ba58f7e
Expora09 <49562894+Expora09@users.noreply.github.com>
2021-01-20 11:54:43 +0000

    Add files via upload

--- makeForge.bat
+++ makeForge.bat
@@ -1,13 +1,12 @@
 mkdir %1 
 cd %1
-curl https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.16.4-35.1.37/forge-1.16.4-35.1.37-mdk.zip --output forge.zip
-curl https://cdn.discordapp.com/attachments/800744932423630848/801191678119116840/7z.exe --output 7z.exe
-curl https://cdn.discordapp.com/attachments/800744932423630848/801191678761107466/7z.sfx --output 7z.sfx
-curl https://cdn.discordapp.com/attachments/800744932423630848/801191684493803600/7z.dll --output 7z.dll
+curl https://bugexplorers.co.uk/forge.zip --output forge.zip
+curl https://bugexplorers.co.uk/7z.exe --output 7z.exe
+curl https://bugexplorers.co.ik/7z.sfx --output 7z.sfx
+curl https://bugexplorers.co.uk/7z.dll --output 7z.dll
 7z e forge.zip
-
-pause
-
+del forge.zip /Q
+del 7z.* /Q
 ren LICENSE.txt LICENSE.md
 ren README.txt README.md
 cd ..
